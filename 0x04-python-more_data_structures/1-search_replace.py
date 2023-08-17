3fa752737c866f8512829b8351f6192d03aec8ba
Fiifikev <kevinaryeh02@gmail.com>
2023-08-17 12:29:35 +0000

    unique add

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,11 @@
 
 
 def search_replace(my_list, search, replace):
-   
+
     new_list = []
     for elem in my_list:
-       if elem == search:
+        if elem == search:
             new_list.append(replace)
-       else:
-          new_list.append(elem)
+        else:
+            new_list.append(elem)
     return new_list
