3f4389145b5c4e6e543d1934e616797c4d1700c9
Fiifikev <kevinaryeh02@gmail.com>
2024-03-18 08:38:11 -0500

    error fix on print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-
-
 def print_matrix_integer(matrix=[[]]):
-for m  in matrix:
-	for i  in m:
-		print("{5:3d".format(i), end ='')
-	print()
-	if not matrix:
-		return 
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
