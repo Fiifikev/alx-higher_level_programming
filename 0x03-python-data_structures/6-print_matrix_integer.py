18728bb1b20db95aad215595463baedbdc7154af
Fiifikev <kevinaryeh02@gmail.com>
2024-03-18 22:46:28 -0500

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
+#!/usr/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j != (len(matrix[i]) - 1):
-                print(" ", end="")
-        print("")
+        for eachrow in matrix:
+            for eachcol in eachrow:
+                print("{:d}".format(eachcol),
+                    end=" " if eachcol != eachrow[-1] else "")
+            print()
