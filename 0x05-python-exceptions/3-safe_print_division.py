0585461a791786d7a096429ece25cb77a21ca252
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 09:42:36 -0500

    safe print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
+    division = 0
     try:
         division = a / b
-    except (TypeError, ZeroDivisionError):
+    except (ZeroDivisionError):
         division = None
     finally:
         print("Inside result: {}".format(division))
