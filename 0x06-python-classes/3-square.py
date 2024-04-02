ed467f3454e6e8482f25c0a8113435f4c2875ce3
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 14:51:55 -0500

    ares of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """This class implements a  Square that defines a square"""
+
+    def __init__(self, size=0):
+        """this is the class constructor
+        Args:
+            size: the input size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            self.__size = size
+
+    def area(self):
+        """ Calculate the area of the square"""
+        return (self.__size * self.__size)
