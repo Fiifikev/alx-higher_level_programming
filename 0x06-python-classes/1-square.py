8a5c80a4dc9e88519e42a14b4e09461d4cd124ae
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 14:05:17 -0500

    sqaure.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a private class instance and a constructor"""
+
+
+class Square:
+        """This class implements an empty Square that defines a square"""
+
+        def __init__(self, size):
+            """this is the class constructor
+            Args:
+                size: the input size of the square.
+            """
+            self.__size = size
