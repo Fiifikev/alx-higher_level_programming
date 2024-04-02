5ae806e9165a11b4471c7dd5e7788cb62164eeac
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 10:54:16 -0500

    magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    calc = 0
+    for num in range(1, 3):
+        try:
+            if num > a:
+                raise Exception("Too far")
+            else:
+                calc += pow(a, b) / i
+        except Exception:
+            calc = a + b
+            break
+    return calc
