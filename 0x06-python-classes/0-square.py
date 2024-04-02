c92880331191015e3d04e8cf85bb4f4f7f333102
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 14:13:44 -0500

    square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+"""Empty class for the code."""
 
-"""Empty class."""
 
 class Square:
-    """ Empty class Square that defines a square."""
-    pass
+    """The empty Square that defines a square"""
