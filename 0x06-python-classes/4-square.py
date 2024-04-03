e4d016f9ce9511c7a509c1ecd69594fe3f823984
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 23:04:04 -0500

    access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""Emulates a square"""
+
+
+"""Define a class Square."""
 
 
 class Square:
-    """This class implements a  Square that defines a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """this is the class constructor
-        Args:
-            size: the input size of the square.
-        """
-    if type(size) != int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        """A new square.
 
-    def area(self):
-        """ Calculate the area of the square"""
-        return self.__size * self.__size
+    Args:
+        size (int): The size of the new square.
+    """
+        self.size = size
 
     @property
     def size(self):
-        """ a public setter metter to retrieve the size"""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ the mutator that set a size for us"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-    self.__size = value
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
