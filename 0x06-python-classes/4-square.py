b9f047a8a7d872e00bd6bfb6a9a15f586f61f532
Fiifikev <kevinaryeh02@gmail.com>
2024-04-02 21:53:39 -0500

    accessnprivate attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Emulates a square"""
+
+
+class Square:
+    """This class implements a  Square that defines a square"""
+
+    def __init__(self, size=0):
+        """this is the class constructor
+        Args:
+            size: the input size of the square.
+        """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+
+    def area(self):
+        """ Calculate the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ a public setter metter to retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ the mutator that set a size for us"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+    self.__size = value
