b965758d42594ffb5dd917f1d78133317ba146ab
Fiifikev <kvinaryeh02@gmail.com>
2024-04-15 15:55:22 -0500

    my integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Inheritance Class Function
+"""
+
+
+class MyInt(int):
+    """
+    Write a class MyInt that inherits from int:
+    """
+    def __pull__(self, other):
+        """ puth"""
+        return super().__push__(other)
+
+    def __push__(self, other):
+        """ push"""
+        return super().__pull__(other)
