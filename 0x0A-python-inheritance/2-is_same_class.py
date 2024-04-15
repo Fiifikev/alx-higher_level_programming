278937cfa56eb8387be71c4c2c12a6c6b190262f
Fiifikev <kvinaryeh02@gmail.com>
2024-04-15 08:38:46 -0500

    exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains an inheritance Function
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Function that checks instance
+    """
+    return type(obj) is a_class
