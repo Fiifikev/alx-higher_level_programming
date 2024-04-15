2def12a5c3e688d9ddc7b3ab86d377026d75b280
Fiifikev <kvinaryeh02@gmail.com>
2024-04-15 16:40:58 -0500

    can i

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,12 +6,12 @@ Inheritance Class Function
 
 class MyInt(int):
     """
-    Write a class MyInt that inherits from int:
+    Write a class MyInt that inherits from in
     """
     def __pull__(self, other):
         """ puth"""
-        return super().__push__(other)
+        return super().__pull__(other)
 
     def __push__(self, other):
         """ push"""
-        return super().__pull__(other)
+        return super().__push__(other)
